b0b10ec7708db0d2da7d1627ee3b91becd512d18
gmachadom <gmm.snf22@uea.edu.br>
2025-04-27 13:58:22 -0400

    Privatized attributes and methods; Added blockchain validation

--- block.py
+++ block.py
@@ -4,33 +4,33 @@ import hashlib
 
 class Block:
     def __init__(self, index, timestamp, data, prev_hash, voter_hash):
-        self.index = index
-        self.timestamp = timestamp
-        self.data = data
-        self.prev_hash = prev_hash
-        self.nonce = 0
-        self.hash = self.compute_hash()
-        self.voter_hash = voter_hash
+        self.__index = index
+        self.__timestamp = timestamp
+        self.__data = data
+        self.__prev_hash = prev_hash
+        self.__nonce = 0
+        self.__hash = self.compute_hash()
+        self.__voter_hash = voter_hash
 
     def compute_hash(self):
         block_string = json.dumps({
-            'index': self.index,
-            'timestamp': self.timestamp,
-            'data': self.data,
-            'prev_hash': self.prev_hash,
-            'nonce': self.nonce
+            'index': self.__index,
+            'timestamp': self.__timestamp,
+            'data': self.__data,
+            'prev_hash': self.__prev_hash,
+            'nonce': self.__nonce
         }, sort_keys=True).encode()
         generated_hash = hashlib.sha256(block_string).hexdigest()
-        print(generated_hash, "Nonce:", self.nonce)
+        print(generated_hash, "Nonce:", self.__nonce)
 
         return generated_hash
 
     def proof_of_work(self, difficulty):
         target = "0" * difficulty
 
-        while not self.hash.startswith(target):
-            self.nonce += 1
-            self.hash = self.compute_hash()
+        while not self.__hash.startswith(target):
+            self.__nonce += 1
+            self.__hash = self.compute_hash()
 
     def get_voter_hash(self):
-        return self.voter_hash
+        return self.__voter_hash
--- blockchain.py
+++ blockchain.py
@@ -11,76 +11,44 @@ FILENAME = "blockchain.json"
 def is_new_block_valid(block, previous_block):
     return (block
             and previous_block
-            and previous_block.index + 1 == block.index
-            and block.prev_hash == previous_block.hash
-            and block.hash == block.compute_hash())
+            and previous_block.__index + 1 == block.__index
+            and block.__prev_hash == previous_block.__hash
+            and block.__hash == block.compute_hash())
 
 
 class Blockchain:
     def __init__(self, difficulty):
-        self.difficulty = difficulty
-        self.chain = []
-        self.start_blockchain()
+        self.__difficulty = difficulty
+        self.__chain = []
+        self.__start_blockchain()
 
-    def get_latest_block(self):
-        return self.chain[-1]
+    def __get_latest_block(self):
+        return self.__chain[-1]
 
-    def start_blockchain(self):
-        try:
-            self.load_from_file()
-        except FileNotFoundError:
-            self.create_genesis_block()
-
-    def create_genesis_block(self):
+    def __create_genesis_block(self):
         genesis_block = Block(0, time(), "0", None, None)
-        genesis_block.proof_of_work(self.difficulty)
-        self.chain.append(genesis_block)
+        genesis_block.proof_of_work(self.__difficulty)
+        self.__chain.append(genesis_block)
 
         print("Bloco gênesis criado.")
 
-    def new_block(self, data, voter_hash):
-        latest_block = self.get_latest_block()
-        return Block(latest_block.index + 1, time(), data, latest_block.hash, voter_hash)
+    def __is_blockchain_valid(self):
+        def is_genesis_block_valid():
+            genesis_block = self.__chain[0]
+            return (genesis_block
+                    and genesis_block.__index == 0
+                    and genesis_block.__prev_hash is None
+                    and genesis_block.__hash == genesis_block.compute_hash())
+        if not is_genesis_block_valid():
+            return False
 
-    def add_block(self, data, voter_hash):
-        new_block = self.new_block(data, voter_hash)
+        for i in range(1, len(self.__chain)):
+            if not is_new_block_valid(self.__chain[i], self.__chain[i - 1]):
+                return False
 
-        if new_block and is_new_block_valid(new_block, self.get_latest_block()):
-            new_block.proof_of_work(self.difficulty)
-            self.chain.append(new_block)
+        return True
 
-        print("Bloco adicionado à blockchain.")
-
-        self.save_to_file()
-
-    def get_chain(self):
-        return [vars(block) for block in self.chain]
-
-    def get_categories(self):
-        categories = set()
-        for block in self.chain:
-            if isinstance(block.data, dict):
-                categories.add(block.data["voto"])
-
-        return categories
-
-    def get_votes(self):
-        votes = defaultdict(int)
-
-        for block in self.chain:
-            if isinstance(block.data, dict):
-                votes[block.data["voto"]] += 1
-
-        return votes
-
-    def has_user_voted(self, voter_hash):
-        for block in self.chain:
-            if block.get_voter_hash() == voter_hash:
-                return True
-        
-        return False
-
-    def save_to_file(self):
+    def __save_to_file(self):
         blockchain_json = json.dumps(self.get_chain(), indent=4)
 
         try:
@@ -91,7 +59,7 @@ class Blockchain:
                 file.write(blockchain_json)
         print("Blockchain salva com sucesso.")
 
-    def load_from_file(self):
+    def __load_from_file(self):
         if not os.path.exists(FILENAME):
             print("Arquivo não encontrado.")
             raise FileNotFoundError
@@ -99,7 +67,7 @@ class Blockchain:
         with open(FILENAME, "r") as file:
             loaded_chain = json.load(file)
 
-        self.chain = []
+        self.__chain = []
         for block_data in loaded_chain:
             block = Block(
                 index=block_data['index'],
@@ -108,8 +76,58 @@ class Blockchain:
                 prev_hash=block_data['prev_hash'],
                 voter_hash=block_data['voter_hash']
             )
-            block.nonce = block_data['nonce']
-            block.hash = block_data['hash']
-            self.chain.append(block)
+            block.__nonce = block_data['nonce']
+            block.__hash = block_data['hash']
+            self.__chain.append(block)
 
         print("Blockchain carregada com sucesso.")
+
+
+    def __start_blockchain(self):
+        try:
+            self.__load_from_file()
+            self.__is_blockchain_valid()
+        except FileNotFoundError:
+            self.__create_genesis_block()
+
+    def __new_block(self, data, voter_hash):
+        latest_block = self.__get_latest_block()
+        return Block(latest_block.__index + 1, time(), data, latest_block.__hash, voter_hash)
+
+    def add_block(self, data, voter_hash):
+        new_block = self.__new_block(data, voter_hash)
+
+        if new_block and is_new_block_valid(new_block, self.__get_latest_block()):
+            new_block.proof_of_work(self.__difficulty)
+            self.__chain.append(new_block)
+
+        print("Bloco adicionado à blockchain.")
+
+        self.__save_to_file()
+
+    def get_chain(self):
+        return [vars(block) for block in self.__chain]
+
+    def get_categories(self):
+        categories = set()
+        for block in self.__chain:
+            if isinstance(block.__data, dict):
+                categories.add(block.__data["voto"])
+
+        return categories
+
+    def get_votes(self):
+        votes = defaultdict(int)
+
+        for block in self.__chain:
+            if isinstance(block.__data, dict):
+                votes[block.__data["voto"]] += 1
+
+        return votes
+
+    def has_user_voted(self, voter_hash):
+        for block in self.__chain:
+            if block.get_voter_hash() == voter_hash:
+                return True
+        
+        return False
